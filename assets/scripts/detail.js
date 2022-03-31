79187ba0d51b22cd7b76a1df618f55000c08a507
Khang Nguyen <nguyenduykhanghuflit@gmail.com>
2022-03-31 15:06:43 +0700

    test 2

--- assets/scripts/detail.js
+++ assets/scripts/detail.js
@@ -3,21 +3,3 @@ function clickme(smallImg) {
   var fullImg = document.getElementById('imagebox');
   fullImg.src = smallImg.src;
 }
-
-let btnMinus = document.querySelector('.button-minus');
-let btnAdd = document.querySelector('.button-add');
-let amout = document.querySelector('.input-amout');
-
-//nút thêm bớt sản phẩm
-btnMinus.addEventListener('click', (e) => {
-  let currentVal = amout.value;
-  if (currentVal > 1) {
-    let newVal = currentVal - 1;
-    amout.value = newVal;
-  }
-});
-btnAdd.addEventListener('click', (e) => {
-  let currentVal = amout.value;
-  let newVal = parseInt(currentVal) + 1;
-  amout.value = newVal;
-});
--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -1,27 +1,55 @@
-let formLogin = document.querySelector('.header-top-login-box');
-let fromResetPassword = document.querySelector('.reset-password-box');
+// let formLogin = document.querySelector('.header-top-login-box');
+// let fromResetPassword = document.querySelector('.reset-password-box');
 
-let btnResetPassword = document.getElementById('modal-reset-password');
-let btnBackLogin = document.getElementById('modal-back-login');
+// let btnResetPassword = document.getElementById('modal-reset-password');
+// let btnBackLogin = document.getElementById('modal-back-login');
 
-let headerModal = document.querySelectorAll('.header-top-modal');
-let a = document.getElementById('app');
-btnResetPassword.addEventListener('click', (e) => {
-  formLogin.style.display = 'none';
-  fromResetPassword.style.display = 'block';
-});
+// let headerModal = document.querySelectorAll('.header-top-modal');
+// let a = document.getElementById('app');
+// btnResetPassword.addEventListener('click', (e) => {
+//   formLogin.style.display = 'none';
+//   fromResetPassword.style.display = 'block';
+// });
 
-btnBackLogin.addEventListener('click', (e) => {
-  formLogin.style.display = 'block';
-  fromResetPassword.style.display = 'none';
-});
+// btnBackLogin.addEventListener('click', (e) => {
+//   formLogin.style.display = 'block';
+//   fromResetPassword.style.display = 'none';
+// });
 
-if (fromResetPassword.style.display == 'block') {
-  alert('dang block');
-}
+let btnMinus = document.querySelectorAll('.button-minus');
+let btnAdd = document.querySelectorAll('.button-add');
+let amout = document.querySelectorAll('.input-amout');
+let totalMoney = document.querySelectorAll('.total-money');
+let priceProducts = document.querySelectorAll('.info-price');
 
-// if (formLogin.style.display == 'block') {
-//   a.addEventListener(click, (e) => {
-//     formLogin.style.display = 'none';
+// const TotalMoney = () => {
+//   let total = 0;
+//   priceProducts.forEach((money) => {
+//     let price = parseInt(
+//       money.textContent.slice(0, money.textContent.length - 1)
+//     );
+//     total += price;
 //   });
-// }
+//   console.log(total);
+// };
+
+for (let i = 0; i < btnMinus.length; i++) {
+  let priceProduct = priceProducts[i].textContent;
+  let price = parseInt(priceProduct.slice(0, priceProduct.length - 1));
+  btnMinus[i].addEventListener('click', (e) => {
+    let currentVal = amout[i].value;
+    if (currentVal > 1) {
+      // TotalMoney();
+      let newVal = currentVal - 1;
+      amout[i].value = newVal;
+      totalMoney[i].innerHTML = `${newVal * price} ₫`;
+    }
+  });
+  btnAdd[i].addEventListener('click', (e) => {
+    // TotalMoney();
+    let currentVal = amout[i].value;
+    let newVal = parseInt(currentVal) + 1;
+    amout[i].value = newVal;
+    totalMoney[i].innerHTML = `${newVal * price} ₫`;
+  });
+}
