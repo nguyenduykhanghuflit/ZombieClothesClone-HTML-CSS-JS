3590d409178e71f4c617f59a1fac404f8e2a37fa
Khang Nguyen <nguyenduykhanghuflit@gmail.com>
2022-04-07 22:40:26 +0700

    fix bug js

--- assets/scripts/detail.js
+++ assets/scripts/detail.js
@@ -1,11 +1,1 @@
-//Chi tiết hình ảnh
-function clickme(smallImg) {
-  var fullImg = document.getElementById('imagebox');
-  fullImg.src = smallImg.src;
-}
 
-//thêm vào giỏ hàng
-
-//xóa sản phẩm trong giỏ hàng
-
-//tính tiền giỏ hàng
