bb1c4567eb7e7c9ec28352da62822a25e9cd600e
Khang Nguyen <nguyenduykhanghuflit@gmail.com>
2022-04-09 13:10:30 +0700

    test local

--- assets/scripts/detail.js
+++ assets/scripts/detail.js
@@ -1,8 +1,8 @@
-
 //--------------------------DETAIL-----------------------------
 
 //---xử lý chọn size---
 let btnSizes = document.querySelectorAll('.detail-size .size'); //mấy cái nút size
+let btnAddCart = document.querySelector('.detail-button-add-cart');
 
 const removeAllSizeActive = (btnSizes) => {
   btnSizes.forEach((size) => {
@@ -26,3 +26,24 @@ const getSize = () => {
   });
   return selectSize;
 };
+
+let price = document.querySelector('.total-money').textContent;
+
+btnAddCart.addEventListener('click', (e) => {
+  let size = getSize();
+  if (size != null) {
+    let img = document.querySelector('#imagebox').src;
+    let totalMoney = document.querySelector('.total-money').textContent;
+    let title = document.querySelector('.detail-title').textContent;
+    let amount = document.querySelector('.input-amout').value;
+    let data = [{ title, price, totalMoney, size, img, amount }];
+    console.log(data);
+  } else console.log('bạn chưa chọn size');
+});
+
+//lấy giá trị từ localStorage với key=cart
+//let itemCart=value
+//itemCart.push(data);
+//push lại vào localStorage;
+
+//[{},{},{}]
